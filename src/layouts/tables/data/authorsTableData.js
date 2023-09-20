721f8642c9950f63e734580299423d772d31564c
ifeanyi305 <otijoseph305@gmail.com>
2023-09-20 17:08:32 +0100

    Create member's list table

--- src/layouts/tables/data/authorsTableData.js
+++ src/layouts/tables/data/authorsTableData.js
@@ -41,22 +41,88 @@ function Function({ job, org }) {
 const authorsTableData = {
   columns: [
     { name: "users", align: "left" },
-    { name: "number", align: "left" },
+    { name: "userID", align: "center" },
+    { name: "number", align: "center" },
+    { name: "email", align: "center" },
+    { name: "totalWagered", align: "center" },
+    { name: "totalGCR", align: "center" },
+    { name: "chatMessages", align: "center" },
+    { name: "walletBalance", align: "center" },
+    { name: "RegisteredDate", align: "center" },
+    { name: "FirstDepositDate", align: "center" },
+    { name: "LastDepositDate", align: "center" },
+    { name: "LastLoginDate", align: "center" },
+    { name: "LastLoginIP", align: "center" },
+    { name: "Chat", align: "center" },
     { name: "status", align: "center" },
-    { name: "referalID", align: "center" },
     { name: "action", align: "center" },
   ],
 
   rows: [
     {
       users: <Author image={team2} name="John Michael" email="john@valiant_joe.com" />,
-      referalID: <Function job="sqz11829sgh" org="P2E" />,
+      userID: <Function job="sqz11829sgh" />,
       status: (
         <SoftBadge variant="gradient" badgeContent="online" color="success" size="xs" container />
       ),
       number: (
         <SoftTypography variant="caption" color="secondary" fontWeight="medium">
          07089283903
+        </SoftTypography>
+      ),
+      email: (
+        <SoftTypography variant="caption" color="secondary" fontWeight="medium">
+         john@gmail.com
+        </SoftTypography>
+      ),
+      totalWagered: (
+        <SoftTypography variant="caption" color="secondary" fontWeight="medium">
+         102345
+        </SoftTypography>
+      ),
+      totalGCR: (
+        <SoftTypography variant="caption" color="secondary" fontWeight="medium">
+         1023.48
+        </SoftTypography>
+      ),
+      chatMessages: (
+        <SoftTypography variant="caption" color="secondary" fontWeight="medium">
+         Hi there!!!
+        </SoftTypography>
+      ),
+      walletBalance: (
+        <SoftTypography variant="caption" color="secondary" fontWeight="medium">
+          $345
+        </SoftTypography>
+      ),
+      RegisteredDate: (
+        <SoftTypography variant="caption" color="secondary" fontWeight="medium">
+          03/08/2023, 23:48:14
+        </SoftTypography>
+      ),
+      FirstDepositDate: (
+        <SoftTypography variant="caption" color="secondary" fontWeight="medium">
+          02/09/2023, 13:48:14
+        </SoftTypography>
+      ),
+      LastDepositDate: (
+        <SoftTypography variant="caption" color="secondary" fontWeight="medium">
+          03/10/2023, 20:48:14
+        </SoftTypography>
+      ),
+      LastLoginDate: (
+        <SoftTypography variant="caption" color="secondary" fontWeight="medium">
+          04/10/2023, 20:48:14
+        </SoftTypography>
+      ),
+      LastLoginIP: (
+        <SoftTypography variant="caption" color="secondary" fontWeight="medium">
+          234rtpd123
+        </SoftTypography>
+      ),
+      Chat: (
+        <SoftTypography variant="caption" color="secondary" fontWeight="medium">
+          <button>chat on whatsapp</button>
         </SoftTypography>
       ),
       action: (
@@ -73,13 +139,68 @@ const authorsTableData = {
     },
     {
       users: <Author image={"https://img2.nanogames.io/avatar/25671/s"} name="Jaeme Michael" email="jaeme@valiant_joe.com" />,
-      referalID: <Function job="jtaz823n" org="P2E" />,
+      userID: <Function job="jtaz823n" />,
       status: (
         <SoftBadge variant="gradient" badgeContent="online" color="success" size="xs" container />
       ),
       number: (
         <SoftTypography variant="caption" color="secondary" fontWeight="medium">
          090672663547
+        </SoftTypography>
+      ),
+      email: (
+        <SoftTypography variant="caption" color="secondary" fontWeight="medium">
+         johnDoe@gmail.com
+        </SoftTypography>
+      ),
+      totalWagered: (
+        <SoftTypography variant="caption" color="secondary" fontWeight="medium">
+         1016275
+        </SoftTypography>
+      ),
+      totalGCR: (
+        <SoftTypography variant="caption" color="secondary" fontWeight="medium">
+          34923.48
+        </SoftTypography>
+      ),
+      chatMessages: (
+        <SoftTypography variant="caption" color="secondary" fontWeight="medium">
+         Hi there!!!
+        </SoftTypography>
+      ),
+      walletBalance: (
+        <SoftTypography variant="caption" color="secondary" fontWeight="medium">
+          $500
+        </SoftTypography>
+      ),
+      RegisteredDate: (
+        <SoftTypography variant="caption" color="secondary" fontWeight="medium">
+          06/10/2023, 23:48:14
+        </SoftTypography>
+      ),
+      FirstDepositDate: (
+        <SoftTypography variant="caption" color="secondary" fontWeight="medium">
+          01/02/2023, 10:48:14
+        </SoftTypography>
+      ),
+      LastDepositDate: (
+        <SoftTypography variant="caption" color="secondary" fontWeight="medium">
+          03/11/2023, 21:48:14
+        </SoftTypography>
+      ),
+      LastLoginDate: (
+        <SoftTypography variant="caption" color="secondary" fontWeight="medium">
+          02/11/2023, 20:42:14
+        </SoftTypography>
+      ),
+      LastLoginIP: (
+        <SoftTypography variant="caption" color="secondary" fontWeight="medium">
+          290rtpd123
+        </SoftTypography>
+      ),
+      Chat: (
+        <SoftTypography variant="caption" color="secondary" fontWeight="medium">
+          <button>chat on whatsapp</button>
         </SoftTypography>
       ),
       action: (
@@ -96,13 +217,68 @@ const authorsTableData = {
     },
     {
       users: <Author image={team2} name="John Michael" email="john@valiant_joe.com" />,
-      referalID: <Function job="quasi19" org="normal" />,
+      userID: <Function job="quasi19" />,
       status: (
         <SoftBadge variant="gradient" badgeContent="offline" color="secondary" size="xs" container />
       ),
       number: (
         <SoftTypography variant="caption" color="secondary" fontWeight="medium">
          08169937218
+        </SoftTypography>
+      ),
+      email: (
+        <SoftTypography variant="caption" color="secondary" fontWeight="medium">
+         ifeanyi@gmail.com
+        </SoftTypography>
+      ),
+      totalWagered: (
+        <SoftTypography variant="caption" color="secondary" fontWeight="medium">
+         112345
+        </SoftTypography>
+      ),
+      totalGCR: (
+        <SoftTypography variant="caption" color="secondary" fontWeight="medium">
+         11203.48
+        </SoftTypography>
+      ),
+      chatMessages: (
+        <SoftTypography variant="caption" color="secondary" fontWeight="medium">
+         Hi there!!!
+        </SoftTypography>
+      ),
+      walletBalance: (
+        <SoftTypography variant="caption" color="secondary" fontWeight="medium">
+          $450
+        </SoftTypography>
+      ),
+      RegisteredDate: (
+        <SoftTypography variant="caption" color="secondary" fontWeight="medium">
+          03/10/2023, 13:48:14
+        </SoftTypography>
+      ),
+      FirstDepositDate: (
+        <SoftTypography variant="caption" color="secondary" fontWeight="medium">
+          01/09/2023, 16:48:14
+        </SoftTypography>
+      ),
+      LastDepositDate: (
+        <SoftTypography variant="caption" color="secondary" fontWeight="medium">
+          04/10/2023, 20:48:14
+        </SoftTypography>
+      ),
+      LastLoginDate: (
+        <SoftTypography variant="caption" color="secondary" fontWeight="medium">
+          04/10/2023, 20:48:14
+        </SoftTypography>
+      ),
+      LastLoginIP: (
+        <SoftTypography variant="caption" color="secondary" fontWeight="medium">
+          234rtpd121
+        </SoftTypography>
+      ),
+      Chat: (
+        <SoftTypography variant="caption" color="secondary" fontWeight="medium">
+          <button>chat on whatsapp</button>
         </SoftTypography>
       ),
       action: (
@@ -117,7 +293,6 @@ const authorsTableData = {
         </SoftTypography>
       ),
     },
-  
   ],
 };
 
